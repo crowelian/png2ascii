2c0c5926160152a8fbef69d5d4d01e42ad4556b6
crowelian-w <harricgi@gmail.com>
2023-03-14 09:50:12 +0200

    fix project settings and problems

---
+++ src/main/java/com/crowelian/App.java
@@ -0,0 +1,13 @@
+package com.crowelian;
+
+/**
+ * png2ascii
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello ascii!" );
+    }
+}
